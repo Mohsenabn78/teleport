32421e3a281ac6ddfe44d4835fc860e3771b6046
mohsen <mohsenabedini79OO@gmail.com>
2023-04-11 16:13:49 +0430

    add dokka dokka and javadoc

---
+++ build/dokka/javadoc/module-search-index.js
@@ -0,0 +1,1 @@
+var moduleSearchIndex = [{"l":"teleport","url":"index.html"}]
---
+++ build/dokka/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+var packageSearchIndex = [{"l":"[root]","url":"[root]/package-summary.html"}, {"l":"All packages","url":"index.html"}]
